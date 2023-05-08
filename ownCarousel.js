2ab804cf947442258f8d645557a63b829e70214b
bajo3 <Felipelentini98@hotmail.com>
2023-05-08 16:56:40 -0300

    first commit

---
+++ ownCarousel.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.owl-carousel').owlCarousel({
+      margin: 10,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true,
+      loop: true,
+      nav: true,
+      navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+      responsive: {
+          0: { items: 1 },
+          600: { items: 4 }
+      }
+  });
+});

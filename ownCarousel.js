0e1d8f14ca907c7289ddec129af0436c53c9f1df
bajo3 <Felipelentini98@hotmail.com>
2023-05-09 14:29:42 -0300

    Reponsivisando

--- ownCarousel.js
+++ ownCarousel.js
@@ -1,15 +1,29 @@
 $(function() {
-  $('.owl-carousel').owlCarousel({
+  $('.owl-carousel-4').owlCarousel({
       margin: 10,
       autoplay: true,
       autoplayTimeout: 2000,
       autoplayHoverPause: true,
       loop: true,
-      nav: true,
+      nav: false,
       navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
       responsive: {
           0: { items: 1 },
           600: { items: 4 }
+      }
+  });
+
+  $('.owl-carousel-3').owlCarousel({
+      margin: 10,
+      autoplay: true,
+      autoplayTimeout: 3000,  
+      autoplayHoverPause: true,
+      loop: true,
+      nav: false,
+      navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+      responsive: {
+          0: { items: 1 },
+          600: { items: 3 }
       }
   });
 });
